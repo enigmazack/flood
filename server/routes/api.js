1ed6f8ac34c44eb15893d4278a308b0bdfe605ed
enigmaz <enigmatracker@gmail.com>
2019-12-06 10:53:27 +0800

    disk api removed

--- server/routes/api.js
+++ server/routes/api.js
@@ -91,8 +91,4 @@ router.get('/traffic', (req, res) => {
   res.json(req.services.historyService.getTransferSummary());
 })
 
-router.get('/disk', (req, res) => {
-    res.json(req.services.diskUsageService.getDiskUsage());
-  })
-
 module.exports = router;
